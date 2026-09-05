e7a7c03a4ee884b8ed2f86441ab699a8829e31dc
ihofmann <ingo.hofmann.privat@gmail.com>
2026-09-05 21:58:33 +0200

    Improve reponsiveness of formation screen

--- websoccer/frontbase.inc.php
+++ websoccer/frontbase.inc.php
@@ -25,7 +25,7 @@ define('PARAM_PAGE', 'page');
 define('PARAM_BLOCK', 'block');
 define('PARAM_PAGENUMBER', 'pageno');
 define('MSG_KEY_ERROR_PAGENOTFOUND', 'error_page_not_found');
-define('ASSETS_VERSION', 'av1788620590');
+define('ASSETS_VERSION', 'av1788638016');
 
 require(BASE_FOLDER . '/admin/config/global.inc.php');
 
